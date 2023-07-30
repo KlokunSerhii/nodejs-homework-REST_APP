b202731dff56ac8370c7e0c2fb366c8b73620bf0
Serhii Klokun <klokuns@gmail.com>
2023-07-30 23:18:37 +0300

    avatarURL

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,14 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const gravatar = require("gravatar");
+const jimp = require("jimp");
+const fs = require("fs/promises");
+const path = require("path");
 const { HttpError, ctrlWrapper } = require("../helpers");
-
 const { User } = require("../models/users");
 
 const { SECRET_KEY } = process.env;
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -15,9 +18,10 @@ const register = async (req, res) => {
     throw HttpError(409, "Email is already in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
-
+  const avatarURL = gravatar.url(email);
   const newUser = await User.create({
     ...req.body,
+    avatarURL,
     password: hashPassword,
   });
 
@@ -30,20 +34,16 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email }, "-createdAt -updatedAt");
-
   if (!user) {
     throw HttpError(401, "Email or password invalid");
   }
-
   const passwordCompare = await bcrypt.compare(
     password,
     user.password
   );
-
   if (!passwordCompare) {
     throw HttpError(401, "Email or password invalid");
   }
-
   const payload = {
     id: user._id,
   };
@@ -85,10 +85,36 @@ const updateSubscriptionUser = async (req, res) => {
   res.json({ _id, subscription });
 };
 
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  if (!_id) {
+    throw HttpError(401);
+  }
+  const { path: tempDir, originalname } = req.file;
+  jimp
+    .read(tempDir)
+    .then((avatar) => {
+      return avatar.cover(250, 250).write(resultUpload);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  const userAvatar = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, userAvatar);
+  await fs.rename(tempDir, resultUpload);
+  const avatarURL = path.join("avatars", userAvatar);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
   updateSubscriptionUser: ctrlWrapper(updateSubscriptionUser),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { validateBody, authenticate } = require("../../middelwares");
+const {
+  validateBody,
+  authenticate,
+  upload,
+} = require("../../middelwares");
 const { schemas } = require("../../models/users");
 const ctrl = require("../../controllers/auth");
 const router = express.Router();
@@ -21,6 +25,13 @@ router.patch(
   authenticate,
   validateBody(schemas.subSchema),
   ctrl.updateSubscriptionUser
+);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
 );
 
 module.exports = router;
