a154bf24a5bd00873675105895159463ed020d31
Serhii Klokun <99805859+KlokunSerhii@users.noreply.github.com>
2023-08-02 16:14:15 +0300

    Update upload.js

--- middelwares/upload.js
+++ middelwares/upload.js
@@ -1,43 +1,17 @@
 const multer = require("multer");
 const path = require("path");
-require('dotenv').config()
 
-const UPLOAD_DIR = path.join(process.cwd(), process.env.UPLOAD_DIR)
+const tempDir = path.join(__dirname, "../", "temp");
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, UPLOAD_DIR)
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-})
+const multerConfig = multer.diskStorage({
+  destination: tempDir,
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
 const upload = multer({
-    storage: storage,
-    limits: { fileSize: 2000000 },
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype.includes('image')) {
-            cb(null, true)
-            return
-        }
-        const err = new Error('Загружена не картинка')
-        err.status = 400
-        cb (err)
-    }})
-
-
-// const tempDir = path.join(__dirname, "../", "temp");
-
-// const multerConfig = multer.diskStorage({
-//   destination: tempDir,
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const upload = multer({
-//   storage: multerConfig,
-// });
+  storage: multerConfig,
+});
 
 module.exports = upload;
